9deee0e3cfa3c76e8135f0b71439fa6cf7805a34
e1pupper <tebaremahmed@gmail.com>
2024-10-17 21:37:36 +0100

    Programming Jesus

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,95 @@
 extern crate proc_macro;
-use core::panic;
 
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Expr, Lit, Meta};
+use syn::{parse_macro_input, Attribute, DeriveInput, Expr, ExprLit, Lit, LitStr, Meta};
 
-#[proc_macro_derive(TeraTemplate, attributes(template))]
-pub fn tera_template_derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident;
+struct Struct<'a> {
+    name: &'a Ident,
+    path: LitStr,
+}
 
-    let attr = input
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("template"))
-        .unwrap();
+#[inline]
+#[must_use]
+fn is_template_attr(attr: &&Attribute) -> bool {
+    attr.path().is_ident("template")
+}
 
-    let template_path = match attr.parse_args() {
-        Ok(Meta::NameValue(meta)) => {
-            if let Expr::Lit(expr) = &meta.value {
-                if let Lit::Str(lit_str) = &expr.lit {
-                    lit_str.value()
-                } else {
-                    panic!("Expected a string literal");
-                }
-            } else {
-                panic!("Expected a string literal");
-            }
-        }
-        _ => panic!("Expected a string literal"),
+fn get_attr(input: &DeriveInput) -> syn::Result<&Attribute> {
+    input.attrs.iter().find(is_template_attr).ok_or_else(|| {
+        syn::Error::new_spanned(input, "The #[template(path = \"...\")] is required.")
+    })
+}
+
+fn get_path_name_value(meta: Meta) -> syn::Result<ExprLit> {
+    let Meta::NameValue(nv) = meta else {
+        return Err(syn::Error::new_spanned(
+            meta,
+            "Expected `path = \"...\"`. E.g. `#[template(path = \"index.html\")]`",
+        ));
     };
 
-    let expanded = quote! {
+    if !nv.path.is_ident("path") {
+        return Err(syn::Error::new_spanned(&nv.path, "Expected `path`."));
+    }
+
+    match nv.value {
+        Expr::Lit(lit) => Ok(lit),
+        _ => Err(syn::Error::new_spanned(
+            nv,
+            "The assignment to `path` must be a string literal.",
+        )),
+    }
+}
+
+fn get_lit_str(lit: ExprLit) -> syn::Result<LitStr> {
+    match lit.lit {
+        Lit::Str(res) => Ok(res),
+        _ => Err(syn::Error::new_spanned(
+            lit,
+            "The assignment to `path` must be a string literal.",
+        )),
+    }
+}
+
+fn get_path_value(input: &Attribute) -> syn::Result<LitStr> {
+    input
+        .parse_args()
+        .and_then(get_path_name_value)
+        .and_then(get_lit_str)
+}
+
+impl<'a> Struct<'a> {
+    fn from_syn(input: &'a DeriveInput) -> syn::Result<Self> {
+        let name = &input.ident;
+        get_attr(input)
+            .and_then(get_path_value)
+            .map(move |path| Self { name, path })
+    }
+}
+
+fn expand_struct(s: Struct) -> proc_macro2::TokenStream {
+    let Struct { name, path } = s;
+
+    quote! {
         impl #name {
             fn render(&self, tera: tera::Tera) -> String {
                 let context = tera::Context::from_serialize(self).expect("Failed to create context");
                 let rendered =  tera
-                    .render(#template_path, &context)
+                    .render(#path, &context)
                     .expect("Failed to render template");
                 rendered
             }
         }
-    };
+    }
+}
 
-    expanded.into()
+#[proc_macro_derive(TeraTemplate, attributes(template))]
+pub fn tera_template_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    Struct::from_syn(&input)
+        .map_or_else(syn::Error::into_compile_error, expand_struct)
+        .into()
 }
